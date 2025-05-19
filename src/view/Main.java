2ac33830fc506fd4001f212e54bd211e10e84b48
yasmine2512 <155321893+yasmine2512@users.noreply.github.com>
2025-05-19 12:13:31 +0100

    Update Main.java

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,12 +1,16 @@
 package view;
 
 import javafx.application.Application;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application{
+
+
 	@Override
     public void start(Stage primaryStage) throws Exception {
         // Charger la vue de connexion (UserView)
@@ -14,7 +18,7 @@ public class Main extends Application{
         Parent root = loader.load();
         
         primaryStage.setTitle("Gestionnaire de Tâches");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 663, 464));
         primaryStage.show();
     }
 
@@ -23,7 +27,14 @@ public class Main extends Application{
         launch(args);
     }
 }
+
 //RUN IN TERMINAL
 //javac --module-path "C:\Users\YASMINE\Downloads\javafx-sdk-23.0.2\lib" --add-modules javafx.controls,javafx.fxml view/Main.java
 //java --module-path "C:\Users\YASMINE\Downloads\javafx-sdk-23.0.2\lib" --add-modules javafx.controls,javafx.fxml view.Main
+//  --module-path "C:\Users\YASMINE\Downloads\javafx-sdk-23.0.2\lib" ^ --add-modules javafx.controls,javafx.fxml ^ --class-path "C:\Users\YASMINE\Downloads\mysql-connector-j-9.2.0\mysql-connector-j-9.2.0.jar;." view.Main
+/*java --module-path "C:\Users\YASMINE\Downloads\javafx-sdk-23.0.2\lib" ^
+--add-modules javafx.controls,javafx.fxml ^
+--class-path "C:\Users\YASMINE\Downloads\mysql-connector-j-9.2.0\mysql-connector-j-9.2.0.jar;." view.Main */
+
+
 
