7c5a910eec529ccc7c7f111a6d8d55e3a954ec2b
yasmine2512 <155321893+yasmine2512@users.noreply.github.com>
2025-05-22 09:13:12 +0100

    Update User.java

--- src/model/User.java
+++ src/model/User.java
@@ -1,20 +1,24 @@
 package model;
 
 public class User {
+
      private int id;
         private String username;
         private String password;
+        private int score;
 
         // Constructeurs, getters et setters
-        public User(int id, String username, String password) {
+        public User(int id, String username, String password, int score) {
             this.id = id;
             this.username = username;
             this.password = password;
+            this.score = score;
         }
 
         public User(String username, String password) {
             this.username = username;
             this.password = password;
+            this.score = 0;
         }
 
         public int getId() {
@@ -40,4 +44,13 @@ public class User {
         public void setPassword(String password) {
             this.password = password;
         }
+        public int getScore() {
+            return score;
+        }
+
+        public void setScore(int score) {
+            this.score = score;
+        }
 }
+
+	
