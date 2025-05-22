bb4d1b5a8221174e98f870eb5c603fc07654c41a
yasmine2512 <155321893+yasmine2512@users.noreply.github.com>
2025-05-22 09:11:46 +0100

    Update Task.java

--- src/model/Task.java
+++ src/model/Task.java
@@ -3,29 +3,36 @@ package model;
 import java.time.LocalDate;
 
 public class Task {
+
      private int id;
         private String title;
         private String description;
         private LocalDate dueDate;
         private String status;
+        private String priority;
+        private String category;
         private int userId;
 
         // Constructeurs, getters et setters
-        public Task(int id, String title, String description, LocalDate dueDate, String status, int userId) {
+        public Task(int id, String title, String description, LocalDate dueDate, String status, int userId,String priority, String category) {
             this.id = id;
             this.title = title;
             this.description = description;
             this.dueDate = dueDate;
             this.status = status;
             this.userId = userId;
+            this.priority = priority;
+            this.category = category;
         }
 
-        public Task(String title, String description, LocalDate dueDate, String status, int userId) {
+        public Task(String title, String description, LocalDate dueDate, String status, int userId,String priority, String category) {
             this.title = title;
             this.description = description;
             this.dueDate = dueDate;
             this.status = status;
             this.userId = userId;
+            this.priority = priority;
+            this.category = category;
         }
 
         public int getId() {
@@ -75,4 +82,9 @@ public class Task {
         public void setUserId(int userId) {
             this.userId = userId;
         }
+        public String getPriority() { return priority; }
+        public void setPriority(String priority) { this.priority = priority; }
+
+        public String getCategory() { return category; }
+        public void setCategory(String category) { this.category = category; }
     }
